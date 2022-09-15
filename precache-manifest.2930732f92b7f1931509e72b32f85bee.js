c23da4dc706a836680a0eeaae8d3f6bb23f233e4
jeppe314 <jesperlindqvist93@gmail.com>
2022-09-15 18:12:14 +0200

    Updates

---
+++ precache-manifest.2930732f92b7f1931509e72b32f85bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7416c9f1420e3be7c721",
+    "url": "/quizzical/static/css/main.925cbc08.chunk.css"
+  },
+  {
+    "revision": "7416c9f1420e3be7c721",
+    "url": "/quizzical/static/js/main.7416c9f1.chunk.js"
+  },
+  {
+    "revision": "aab9aa9e28e0e8b26df1",
+    "url": "/quizzical/static/css/1.8aa3a960.chunk.css"
+  },
+  {
+    "revision": "aab9aa9e28e0e8b26df1",
+    "url": "/quizzical/static/js/1.aab9aa9e.chunk.js"
+  },
+  {
+    "revision": "10828be3562dbc652641",
+    "url": "/quizzical/static/js/runtime~main.10828be3.js"
+  },
+  {
+    "revision": "941946a2431f90b6d38bf48d4b6818a9",
+    "url": "/quizzical/index.html"
+  }
+];
