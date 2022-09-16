f6ae0c79e047434c844af19e220850311a6b39cb
jeppe314 <jesperlindqvist93@gmail.com>
2022-09-16 21:46:20 +0200

    Removed build from .gitignore

---
+++ build/precache-manifest.691174c088019f638cbd325c4c371ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a109b737526dc638c1",
+    "url": "/quizzical/static/css/main.ded94395.chunk.css"
+  },
+  {
+    "revision": "e2a109b737526dc638c1",
+    "url": "/quizzical/static/js/main.e2a109b7.chunk.js"
+  },
+  {
+    "revision": "aab9aa9e28e0e8b26df1",
+    "url": "/quizzical/static/css/1.8aa3a960.chunk.css"
+  },
+  {
+    "revision": "aab9aa9e28e0e8b26df1",
+    "url": "/quizzical/static/js/1.aab9aa9e.chunk.js"
+  },
+  {
+    "revision": "10828be3562dbc652641",
+    "url": "/quizzical/static/js/runtime~main.10828be3.js"
+  },
+  {
+    "revision": "078de2251a6b01c9b0b2f991c0bd7412",
+    "url": "/quizzical/index.html"
+  }
+];
